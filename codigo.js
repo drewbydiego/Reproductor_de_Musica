034348f9686f8a59b370cdf33f0ccec394578607
Diego Flores <diegoflo64411@gmail.com>
2021-05-27 17:50:37 -0600

    Arreglando la barra de volumen & agregando barra de progreso funcional

--- codigo.js
+++ codigo.js
@@ -1,15 +1,38 @@
 const playCancion = document.getElementsByClassName('play');
 const stopCancion = document.getElementsByClassName('stop');
 const volumen = document.querySelector('.controlV');
-let audio;
+const barra_progreso = document.querySelector('#progresocancion');
+const portada = document.querySelector('.portada');
+const muteSonido = document.querySelector('.mute');
+var audio;
 var estiloPlay,estiloStop;
 var stopPresionado = false;
 var playPresionado = false;
 var reproduciendo = 0;
-volumen.addEventListener('change', function(){
+var intervalo;
+var duracion;
+var progreso;
+
+muteSonido.addEventListener('click',()=>{
+    if(audio.muted == false){
+        audio.muted =true;
+        muteSonido.innerHTML = "volume_off"
+    }else{
+        audio.muted = false;
+        muteSonido.innerHTML = "volume_up"
+    }
+});
+
+barra_progreso.addEventListener('change',function(){
+    let progreso = this.value;
+    audio.currentTime= progreso;
+});
+
+volumen.addEventListener('change',function(){
     let vol = this.value;
     audio.volume = vol;
 });
+
 
 //Comprobando si se esta reproduciendo una cancion
 if(reproduciendo == 1){
@@ -26,27 +49,40 @@ for (elemento of playCancion) {
             reproduciendo = 1;
         audio = new Audio(`./audios/${cancion}.mp3`);
         //Comprobando si la cancion ya ha iniciado y si es asi, continuar en su currentTime correspondiente
+        audio.volume = volumen.value;
             audio.play();
             //Pasando el nombre de la cancion como parametro para mostrar la portada
-            portadaCancion(cancion);
+            var intervalo = setInterval(function(){
+                duracion = audio.duration;
+                barra_progreso.max = audio.duration;
+                //console.log(duracion);
+                //barraProgreso(duracion);
+                clearInterval(intervalo);
+            },1000);
+                progreso = setInterval(function(){
+                barra_progreso.value = audio.currentTime;
+                if(audio.currentTime == audio.duration){
+                    console.log('cancion finalizada');
+                    clearInterval(this);
+                }
+            },1000);
         }
         playPresionado = true;
         //Añadiendo la clase que le da color al play
         this.classList.add('cancionIniciada');
         estiloPlay = document.querySelector('.cancionIniciada');
         //Quitando la clase que le da color al stop
+
         if(stopPresionado == true){
             estiloStop.classList.remove('cancionPausa');
         }
 
     });
-
 }
 
 }
-
-function portadaCancion(nombreCancion){
-    console.log(nombreCancion);
+function portadaCancion(/*nombreCancion*/){
+    //console.log(nombreCancion);
 }
 //PARAR LA CANCION
 for (elemento of stopCancion){
@@ -61,8 +97,21 @@ for (elemento of stopCancion){
         estiloStop = document.querySelector('.cancionPausa');
         //Guardando el tiempo que la cancion ha estado reproduciondose
             if(reproduciendo == 1){
+                clearInterval(progreso);
+                barra_progreso.value = 0;
                 audio.pause();
                 reproduciendo = 0;
+                clearInterval(intervalo);
             }
     });
 }
+/*
+function barraProgreso(duracion) {
+    velocidad = duracion / 100;
+    barra_progreso.value = velocidad;
+    console.log(velocidad);
+    intervalo = setInterval(function(){
+        barra_progreso.value = barra_progreso.value + velocidad;
+    },1000);
+    //portadaCancion(audio.duration);
+}*/
